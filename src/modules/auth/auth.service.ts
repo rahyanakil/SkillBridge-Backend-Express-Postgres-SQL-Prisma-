b275a4e4b4af2de5178a86d5dfb7dd704f6d0770
rahyanakil <rahyanakil89@gmail.com>
2026-02-20 19:32:44 +0600

    prisma config and register setup done

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,17 @@
+import bcrypt from "bcryptjs";
+import { prisma } from "../../lib/prisma";
+
 const createUser = async (payload: any) => {
-  console.log(payload);
+  const hashPassword = await bcrypt.hash(payload.password, 8);
+
+  const result = await prisma.user.create({
+    data: {
+      ...payload,
+      password: hashPassword,
+    },
+  });
+  const { password, ...newPassword } = result;
+  return newPassword;
 };
 
 export const AuthService = {
